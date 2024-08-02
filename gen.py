503cf73e1cfb072f271cc0c45edf2303029bc2c8
Dominik Strak <dominikstrak2001@gmail.com>
2024-08-03 00:10:02 +0100

    correct params

--- gen.py
+++ gen.py
@@ -1,9 +1,9 @@
 import argparse
 from problem import Problem
 
-def generate_variants(path, folder, seed: int):
+def generate_variants(path, folder: bool, seed: int):
     problems = (
-        Problem.read_problems_from_dir(folder)
+        Problem.read_problems_from_dir(path)
         if folder
         else [Problem(path)]
     )
@@ -16,12 +16,14 @@ def generate_variants(path, folder, seed: int):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate problems from description. Does not require API keys.')
-    parser.add_argument('--path', type=str, help='a problem path')
-    parser.add_argument('--folder', type=str, help='path to a folder of problems')
+    parser.add_argument('path', type=str, help='a problem path')
+    parser.add_argument(
+        "--folder",
+        "-f",
+        action="store_true",
+        help="Path is the path to problems directory",
+    )
     parser.add_argument('--seed', type=int, help='an optional integer seed', default=0)
     
     args = parser.parse_args()
-    if args.path == None and args.folder == None:
-        print("I need either a --path or --folder to generate problems")
-        exit(1)
     generate_variants(args.path, args.folder, args.seed)
